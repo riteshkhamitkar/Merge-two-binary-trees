528a6452f55c73b5c49622cb787e458e842e46ba
riteshkhamitkar <89683381+riteshkhamitkar@users.noreply.github.com>
2022-07-18 12:33:11 +0530

    Add files via upload

---
+++ Merge two binary trees(L-617).java
@@ -0,0 +1,24 @@
+class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        return merge(t1,t2);
+        
+    }
+    
+    public TreeNode merge(TreeNode t1 , TreeNode t2){
+        if(t1 == null && t2 == null)
+            return null;
+        
+        if(t1 == null && t2 != null)
+            return t2;
+        
+        if(t1 != null && t2 == null)
+            return t1;
+        
+        TreeNode sum = new TreeNode(t1.val + t2.val);
+        
+        sum.left = merge(t1.left , t2.left);
+        sum.right = merge(t1.right , t2.right);
+        
+        return sum;
+    }
+}
